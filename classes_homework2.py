64215fb474e38ec6dc4102d3a63cea5e2248b6ed
Байков Ратмир <rsbaykov@edu.hse.ru>
2026-06-02 20:51:27 +0000

    Добавлен класс Recipe в основной .py файл

--- classes_homework2.py
+++ classes_homework2.py
@@ -24,3 +24,40 @@ class Ingredient:
         if not isinstance(other, Ingredient):
             return False
         return (self.name == other.name and self.unit == other.unit)
+
+
+class Recipe:
+    def __init__(self, title: str, ingredients: list) -> None:
+        self.title = title
+        self.ingredients = ingredients
+    
+    def add_ingredient(self, ingredient: Ingredient) -> None:
+        for ing in self.ingredients:
+            if ing == ingredient:
+                ing.quantity += ingredient.quantity
+            else:
+                self.ingredients.append(ingredient)
+    
+    @staticmethod
+    def is_valid_ratio(ratio) -> bool:
+        if isinstance(ratio, (int, float)):
+            return ratio > 0
+        return False
+    
+    def scale(self, ratio: float) -> Recipe:
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("ratio должен быть положительным числом")
+        mult_ing = []
+        for i in self.ingredients:
+            mult_ing.append(Ingredient(i.name, i.quantity * ratio, i.unit))
+        new_recipe = Recipe(self.title, mult_ing)
+        return new_recipe
+    
+    def __len__(self) -> int:
+        return len(self.ingredients)
+    
+    def __str__(self) -> str:
+        res = f"{self.title}:\n"
+        for i in self.ingredients:
+            res += f"  - {i}\n"
+        return res
